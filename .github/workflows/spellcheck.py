bb637ebb8d25501234fb0a1fa6d06a58839bb047
Gokul Prathin <gokulprathin8@gmail.com>
2024-03-26 22:58:53 -0400

    ignore file to ignore words

--- .github/workflows/spellcheck.py
+++ .github/workflows/spellcheck.py
@@ -2,23 +2,29 @@ from spellchecker import SpellChecker
 import nbformat
 import os
 import sys
+import string  # Import the string module to get a list of punctuation characters
+
+def remove_punctuation(text):
+  translator = str.maketrans('', '', string.punctuation)
+  return text.translate(translator)
 
 def spell_check_notebook(filepath, ignore_words):
   spell = SpellChecker()
   spell.word_frequency.load_words(ignore_words)
 
-  misspelled_words = {}  # Dictionary to collect misspelled words
+  misspelled_words = {}
 
   with open(filepath, 'r', encoding='utf-8') as f:
     nb = nbformat.read(f, as_version=4)
 
   for cell in nb.cells:
     if cell.cell_type == 'markdown':
-      text = cell.source
+      # Preprocess the cell text to remove punctuation before splitting into words
+      text = remove_punctuation(cell.source)
       misspelled = spell.unknown(text.split())
       if misspelled:
         if filepath not in misspelled_words:
-          misspelled_words[filepath] = set()  # Use a set to avoid duplicate words
+          misspelled_words[filepath] = set()
         misspelled_words[filepath].update(misspelled)
 
   return misspelled_words
@@ -39,20 +45,5 @@ def spell_check_directory(directory, ignore_words):
     sys.exit(1)  # Exit with a non-zero status code to indicate failure
 
 if __name__ == "__main__":
-  ignore_list = [
-    'slippy', 'hist', 'NAM', 'gage', 'swe', 'geoweaver', 'workflow', 'datasets', 'snotel',
-    'adeos-ii', 'monthly,', 'products,', 'nsidc', 'separately,', 'control.', '##', 'caps,',
-    'dataset.', 'research**:', 'prediction,', 'atmosphere.', 'amsr?', 'fingertips!', 'risk,',
-    'amsr-related', 'amsr2', 'snowfall,', 'properties.', 'netcdf,', '**cryosphere', 'moisture,',
-    'extent,', 'scheduling.', 'scales.', '1,000m.', 'tourism**:', 'format,', 'temperature,',
-    'purposes,', 'hiking,', '**recreation', 'datasets.', 'workflow,', '(coop),', 'periods.',
-    '[gridmet', '**temporal', '(nam)', 'snow,', 'agriculture,', 'amsr,', 'terrain,', 'beyond.',
-    'data,', '(plain', 'data.', 'u.s.the', '(swe),', 'resolutions:', '**oceanography**:', 'daac',
-    'forestry**:', '*runoff', 'format.', 'hdf5', 'information.', 'instruments.', 'ams/adeos-ii,',
-    'land,', 'typically,', 'reliability,', 'capabilities.', 'gage,', '(network', 'environment.,',
-    'dynamics,', 'trends*:', 'dynamics.', 'hydroclimatic', 'studies*:', 'visualization.', 'degree)',
-    'models:', 'snowboarding,', 'camping.', 'salinity,', 'intervals.', 'multi-disciplinary',
-    'monitoring*:', 'endeavors.', 'columbia', 'conditions.', 'nasa\'s', 'metadata',
-    'snowpacktelemetrynetwork', 'satellite-ii', 'winds,', 'mo', 'fsca', 'modis', 'gridmet'
-  ]
+  ignore_list = []
   spell_check_directory('.', ignore_list)
